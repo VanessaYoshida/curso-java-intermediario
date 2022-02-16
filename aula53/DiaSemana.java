55e141edb9d97c8d653ebdc0ddfe6dfa5b961ca4
VanessaYoshida <vanessayoshida92@gmail.com>
2022-02-16 08:05:17 -0300

    organizando pastas no github

--- aula53/DiaSemana.java
+++
@@ -1,7 +0,0 @@
-package com.vanessa.javaintermediario.aula53;
-
-public enum DiaSemana {
-	
-	SEGUNDA, TERCA, QUARTA, QUINTA, SEXTA, SABADO, DOMINGO;
-	
-}
--- aula64/Texto.java
+++
@@ -1,8 +0,0 @@
-package com.vanessa.javaintermediario.aula64;
-
-public class Texto {
-
-	void imprimeTexto() {
-	}
-	
-}
